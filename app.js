6bc414b3564eabf5943e6cd19e98c5bda22fdd8a
Hassaan-Bandesha <hassaan4742@gmail.com>
2024-05-31 17:14:12 +0500

    Products rendered

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 const div = document.querySelector('div');
+
+
+// Promise Area
+axios('https://fakestoreapi.com/products')
+.then((res)=>{
+    console.log(res.data);
+    res.data.map((item)=>{
+        div.innerHTML += `
+        <div class="card " style="width: 15rem;">
+  <img width="200" src="${item.image}" alt="Product-image">
+  <div class="card-body d-flex justify-content-center text-center align-items-center">
+        <h6>${item.title}</h6>
+        <h6>Price : ${item.price}</h6>
+  </div>
+</div>
+        `
+    })
+}).catch((err)=>{
+    console.log(err);
+})
+// Promise Area
+
+{/* <p class="card-text">${item.description}</p> */}
